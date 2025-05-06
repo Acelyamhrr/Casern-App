18d7b63cc633dd30301f870c88b095993d98a4a7
Diya Boudraa <diya.boudraa@etu.unistra.fr>
2025-05-06 17:17:04 +0200

    connexion.cs et mesDatas.cs

--- SAE-Caserne/SAE-Caserne/Program.cs
+++ SAE-Caserne/SAE-Caserne/Program.cs
@@ -16,7 +16,7 @@ namespace SAE_Caserne
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmAccueil());
         }
     }
 }
---
+++ SAE-Caserne/SAE-Caserne/frmAccueil.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace SAE_Caserne
+{
+    partial class frmAccueil
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmAccueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmAccueil";
+            this.Text = "Les Soldats du feu";
+            this.Load += new System.EventHandler(this.frmAccueil_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
